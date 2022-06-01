79975632e62a42b24c7dc4beed53129b5874b9de
Badi-Haki Martin <jojackblack@gmail.com>
2022-05-31 20:09:29 -0400

    assignment completed

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -18,8 +18,21 @@ function QuestionForm(props) {
   }
 
   function handleSubmit(event) {
+    const answers = [formData.answer1,formData.answer2,formData.answer3,formData.answer4];
+
     event.preventDefault();
     console.log(formData);
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers:
+      { "Content-Type": "application/json" },      
+      body:
+      JSON.stringify({
+        "prompt": formData.prompt,
+        "answers": answers,
+        "correctIndex": formData.correctIndex,
+      })
+    })
   }
 
   return (
--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, questionStateSetter, allQuestions }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,15 +9,56 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleDelete(){
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    }).then(r=>r.json()).then(
+      ()=>{
+        console.log(`deleted ${id}`);
+        questionStateSetter(allQuestions.filter( (question)=>{
+          return question.id !== id;
+        }));
+      }
+    )
+  }
+
+  function handleChangeCorrectAnswer(event){
+    console.log(`correct answer is ${event.target.value}, was ${correctIndex}`)
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type" : "application/json",
+      },
+      body: JSON.stringify({
+        correctIndex: event.target.value,
+    }),
+  }).then(r=>r.json()).then( (updatedQuestion)=>{
+    onUpdatedQuestion(updatedQuestion);
+  })
+}
+
+  function onUpdatedQuestion(updatedQuestion){
+    const updatedQuestionList = allQuestions.map( (quest)=>{
+      if(quest.id === updatedQuestion.id){
+        return updatedQuestion
+      }
+      else{
+        return quest;
+      }
+    });
+
+    questionStateSetter(updatedQuestionList);
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleChangeCorrectAnswer}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
